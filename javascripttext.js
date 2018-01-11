f983b161cb50384eb610b2b91f3dd429e50ede9a
MatthieuClb <matthieu.colombert@gmail.com>
2018-01-11 23:41:20 +0100

    Add files via upload

---
+++ javascripttext.js
@@ -0,0 +1,56 @@
+document.getElementByID("boutonAccueil").onclick = function () {
+
+let btA = document.getElementByID("messageAccueil");
+btA.style.display = "none";
+let mPage = document.getElementByID("milieuPage");
+mPage.style.display = "block";
+
+}
+
+
+
+
+ <script type="text/javascript" src="script.js"></script>
+
+
+
+ document.location.href="https://www.google.fr/#q="recherche";
+
+
+
+
+
+let boutonRecherche = document.getElementById("boutonRecherche");
+let recherche = document.getElementById("champ").value;
+
+
+
+boutonRecherche.onclick = function() {
+
+document.location.href="https://www.google.fr/#q="}
+
+document.location.href="https://www.thehackingproject.org";
+
+
+
+
+
+
+document.location.reload("http://thehackingproject.org")
+
+let boutonRecherche = document.getElementById("boutonRecherche");
+let recherche = document.getElementById("champ").value;
+
+boutonRecherche.onclick = function() {
+
+alert("bravo!")
+}
+
+
+
+
+setTimeout(function() { 
+
+alert("Hey le site xxxvidsxxx est trop bien, Viens dessus stp please"); 
+
+}, 10000);
